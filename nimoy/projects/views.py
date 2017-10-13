205188cafaf13b46339918639bb12f2b2b34493d
Pankaj Chaudhary <pankaj9310@gmail.com>
2017-10-13 21:22:44 +0530

    pie chart added

--- nimoy/projects/views.py
+++ nimoy/projects/views.py
@@ -43,13 +43,16 @@ class ProjectDetails(FormMixin, DetailView):
     form_class = ProjectForm
 
     def _get_tasks(self):
-        import pdb
-        # pdb.set_trace()
-        # CodingChallenge.objects.contest_problems(self.object)
         tasks = Task.objects.filter(project=self.object.pk)
         return [{'name': x.name, 'description': x.description, 'assignee': x.assignee,
                  'task_type': x.task_type, 'status': x.status, 'owner': x.owner, 'priority': x.priority, 'pk': x.id} for x in tasks]
 
+    def _get_graph_data(self):
+        status_data = {'To Do':0, 'Progress':0, 'Completed':0}
+        tasks = Task.objects.filter(project=self.object.pk)
+        for x in tasks:
+            status_data[x.status] += 1
+        return status_data
     def get_success_url(self):
         return reverse('project_detail', kwargs={'pk': self.object.pk})
 
@@ -57,4 +60,5 @@ class ProjectDetails(FormMixin, DetailView):
         context = super(ProjectDetails, self).get_context_data(**kwargs)
         context['form'] = self.form_class(initial={'project': self.object.pk})
         context['tasks'] = self._get_tasks()
+        context['data_values'] = self._get_graph_data().values()
         return context
